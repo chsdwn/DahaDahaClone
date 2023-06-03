ff26ae893c6320c065f645e2ec243a0f0af32043
chsdwn <chasedown@hotmail.com>
2023-06-03 23:09:41 +0300

    fix(App): wrap App root with gesture handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 
+import { theme } from './config/theme';
 import { Home } from './routes';
 import { store } from './store';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Provider store={store}>
-        <SafeAreaView style={styles.container}>
-          <StatusBar />
-          <Home />
-        </SafeAreaView>
-      </Provider>
+      <GestureHandlerRootView style={styles.container}>
+        <Provider store={store}>
+          <SafeAreaView style={styles.container}>
+            <StatusBar backgroundColor={theme.white} barStyle="dark-content" />
+            <Home />
+          </SafeAreaView>
+        </Provider>
+      </GestureHandlerRootView>
     </NavigationContainer>
   );
 };
