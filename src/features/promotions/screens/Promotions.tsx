d8cd3eacab7f020f6cdf43054cfe650a52abbfce
chsdwn <chasedown@hotmail.com>
2023-06-03 18:09:49 +0300

    fix(Promotions): move promotion tags rendering logic

--- src/features/promotions/screens/Promotions.tsx
+++ src/features/promotions/screens/Promotions.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { ScrollView, TouchableWithoutFeedback, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableWithoutFeedback, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import { useSelector } from 'react-redux';
 
 import { Layout, Text } from '@/components';
 import { HomeBottomTabNavigationProp } from '@/types';
@@ -8,14 +9,42 @@ import {
   useGetPromotionsQuery,
   useGetTagsQuery,
 } from '../api/promotionsApiSlice';
-import { PromotionsHeader } from '../components';
+import { PromotionsHeader, TagButtonsList } from '../components';
+import { selectActiveTag } from '../store/promotionsSlice';
+import { IPromotionItem } from '../types';
 
 export const Promotions = () => {
   const navigation = useNavigation<HomeBottomTabNavigationProp>();
 
-  const { data: promotions, isLoading } = useGetPromotionsQuery();
+  const { data: promotionsData, isLoading } = useGetPromotionsQuery();
+  const [promotions, setPromotions] = useState<IPromotionItem[]>([]);
+
   const { data } = useGetTagsQuery();
   const tags = [...(data || [])].sort((a, b) => a.Rank - b.Rank);
+
+  const activeTag = useSelector(selectActiveTag);
+
+  useEffect(() => {
+    if (!promotionsData || promotionsData.length === 0) return;
+    if (!activeTag) {
+      setPromotions(promotionsData);
+      return;
+    }
+
+    const filteredPromotions = promotionsData.filter((promotion) =>
+      promotion.Title.toLocaleLowerCase('tr').includes(
+        activeTag.Name.toLocaleLowerCase('tr'),
+      ),
+    );
+    setPromotions(filteredPromotions);
+  }, [activeTag, promotionsData]);
+
+  const handleNavigatePromotionDetails = (id: number) => {
+    navigation.navigate('PromotionDetailsStack', {
+      screen: 'PromotionDetails',
+      params: { id },
+    });
+  };
 
   if (isLoading) return <Text>Loading...</Text>;
 
@@ -23,30 +52,13 @@ export const Promotions = () => {
     <Layout>
       <PromotionsHeader />
 
-      <Text>Promotion Tags</Text>
-      <View>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {tags.length > 0 &&
-            tags.map((tag) => (
-              <TouchableWithoutFeedback key={tag.Id}>
-                <View style={{ borderWidth: 1, borderColor: 'black' }}>
-                  <Text>{tag.Name}</Text>
-                </View>
-              </TouchableWithoutFeedback>
-            ))}
-        </ScrollView>
-      </View>
+      <TagButtonsList tags={tags} />
 
       <Text>Promotions</Text>
       {promotions?.map((promotion) => (
         <TouchableWithoutFeedback
           key={promotion.Id}
-          onPress={() =>
-            navigation.navigate('PromotionDetailsStack', {
-              screen: 'PromotionDetails',
-              params: { id: promotion.Id },
-            })
-          }
+          onPress={() => handleNavigatePromotionDetails(promotion.Id)}
         >
           <View>
             <Text>{promotion.Title}</Text>
