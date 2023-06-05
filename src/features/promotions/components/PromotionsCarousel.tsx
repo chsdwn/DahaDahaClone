31256065878a110084d91475797a3350aecff403
chsdwn <chasedown@hotmail.com>
2023-06-05 16:16:37 +0300

    fix(PromotionsCarousel): move card component

--- src/features/promotions/components/PromotionsCarousel.tsx
+++ src/features/promotions/components/PromotionsCarousel.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useSharedValue } from 'react-native-reanimated';
 import Carousel from 'react-native-reanimated-carousel';
-import { useNavigation } from '@react-navigation/core';
 
 import { Loading } from '@/components';
 import { WINDOW_WIDTH } from '@/config';
-import { theme } from '@/config/theme';
-import { HomeBottomTabNavigationProp } from '@/types';
-import {
-  BrandIcon,
-  CarouselButton,
-  CarouselCardShadow,
-  CarouselImage,
-  CarouselProgress,
-  CarouselRemainingDaysBadge,
-  CarouselTitle,
-} from '.';
+import { CarouselCard, CarouselProgress } from '.';
 import { IPromotionItem } from '../types';
 
 type IProps = {
@@ -25,16 +14,7 @@ type IProps = {
 };
 
 export const PromotionsCarousel = ({ promotions = [], isLoading }: IProps) => {
-  const navigation = useNavigation<HomeBottomTabNavigationProp>();
-
   const carouselProgress = useSharedValue(0);
-
-  const handleNavigatePromotionDetails = (id: number) => {
-    navigation.navigate('PromotionDetailsStack', {
-      screen: 'PromotionDetails',
-      params: { id },
-    });
-  };
 
   if (isLoading) return <Loading />;
   if (promotions.length === 0) return null;
@@ -57,35 +37,10 @@ export const PromotionsCarousel = ({ promotions = [], isLoading }: IProps) => {
         }}
         data={promotions}
         renderItem={({ item: promotion }) => (
-          <View style={styles.shadowContainer}>
-            <View style={styles.shadow}>
-              <CarouselCardShadow
-                width={WINDOW_WIDTH}
-                color={promotion.PromotionCardColor}
-              />
-            </View>
-
-            <TouchableWithoutFeedback
-              onPress={() => handleNavigatePromotionDetails(promotion.Id)}
-            >
-              <View key={promotion.Id} style={styles.container}>
-                <View style={styles.imageContainer}>
-                  <CarouselImage url={promotion.ImageUrl} />
-
-                  <BrandIcon
-                    backgroundColor={promotion.BrandIconColor}
-                    url={promotion.BrandIconUrl}
-                  />
-                  <CarouselRemainingDaysBadge date={promotion.RemainingText} />
-                </View>
-
-                <CarouselTitle html={promotion.Title} />
-                <CarouselButton html={promotion.ListButtonText} />
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
+          <CarouselCard promotion={promotion} />
         )}
       />
+
       <View style={styles.progressContainer}>
         {!isLoading &&
           promotions.map((p, index) => (
@@ -102,29 +57,7 @@ export const PromotionsCarousel = ({ promotions = [], isLoading }: IProps) => {
   );
 };
 
-const borderRadius = 16;
 const styles = StyleSheet.create({
-  shadowContainer: {
-    marginTop: -WINDOW_WIDTH * 0.08,
-    position: 'relative',
-  },
-  shadow: {
-    position: 'absolute',
-    bottom: -borderRadius,
-    height: borderRadius * 2,
-  },
-  container: {
-    backgroundColor: theme.white,
-    borderColor: theme.lightestGray,
-    borderWidth: 2,
-    borderRadius,
-    paddingBottom: 4,
-  },
-  imageContainer: {
-    margin: 4,
-    borderRadius,
-    position: 'relative',
-  },
   progressContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
